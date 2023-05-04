03f87189deb37954f18b874beaec7164ff3179d2
Akero <rish1994@gmail.com>
2023-05-04 12:22:36 +0530

    test

--- app/src/main/java/com/acme/c/MainActivity.java
+++ app/src/main/java/com/acme/c/MainActivity.java
@@ -25,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        //test
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
